197bfb0849ae80ff83f921bf78e567839b028d49
Juan Pablo <jupapema11@gmail.com>
2026-05-18 16:51:25 -0500

    initial commit

---
+++ src/app/features/activities/components/activity-form/activity-form.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-activity-form',
+  imports: [],
+  templateUrl: './activity-form.html',
+  styleUrl: './activity-form.scss',
+})
+export class ActivityForm {}
---
+++ src/app/features/projects/components/project-summary/project-summary.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-summary',
+  imports: [],
+  templateUrl: './project-summary.html',
+  styleUrl: './project-summary.scss',
+})
+export class ProjectSummary {}
